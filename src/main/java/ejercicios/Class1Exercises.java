a6e25e2927620c65abc867ceb42e7bd6326fa3c8
NeiMeli <nicolas.ortola@mercadolibre.com>
2021-09-05 11:41:42 -0300

    resuelvo ejercicios 6 y 7 y refactorizo

--- src/main/java/ejercicios/Class1Exercises.java
+++ src/main/java/ejercicios/Class1Exercises.java
@@ -15,6 +15,9 @@ public class Class1Exercises {
         exercises.addItem("Ejercicio 3", Class1Exercises::exercise3);
         exercises.addItem("Ejercicio 4", Class1Exercises::exercise4);
         exercises.addItem("Ejercicio 5", Class1Exercises::exercise5);
+        exercises.addItem("Ejercicio 6", Class1Exercises::exercise6);
+        exercises.addItem("Ejercicio 7", Class1Exercises::exercise7);
+
     }
 
     // 1. Ingresar un número entero. En caso de ser positivo indicar ‘true’ por pantalla.
@@ -22,7 +25,7 @@ public class Class1Exercises {
         int num = intInput("Ingrese un numero entero");
 
         if (num > 0) {
-            print(true);
+            printTrue();
         }
     }
 
@@ -32,9 +35,9 @@ public class Class1Exercises {
         int num = intInput("Ingrese un numero entero");
 
         if (num > 0) {
-            print(true);
+            printTrue();
         } else if (num < -10 && num > -20) {
-            print(true);
+            printTrue();
         }
     }
 
@@ -43,7 +46,7 @@ public class Class1Exercises {
         int num = intInput("Ingrese un numero entero");
 
         if (num > 10) {
-            print(true);
+            printTrue();
         }
     }
 
@@ -55,28 +58,80 @@ public class Class1Exercises {
 
         if (a > 0) {
             if (b > 0) {
-                print(true);
+                printTrue();
             }
         } else if (b < 0) {
-            print(true);
+            printTrue();
         }
     }
 
     // 5. Ingresar tres números. Mostrar “true” cada vez que el número ingresado sea par.
     public static void exercise5() {
         int a = intInput("Ingrese un numero entero a");
-        if (a % 2 == 0) {
+        if (isEven(a)) {
             print("a: " + true);
         }
 
         int b = intInput("Ingrese un numero entero b");
-        if (b % 2 == 0) {
+        if (isEven(b)) {
             print("b: " + true);
         }
 
         int c = intInput("Ingrese un numero entero c");
-        if (c % 2 == 0) {
+        if (isEven(c)) {
             print("c: " + true);
         }
+    }
+
+/*    6. Ingresar dos números, 'a' y 'b'. En caso de que ambos sean positivos, para cada uno mostrar 'true' si el resultado
+    de sumarle 1 es par. De modo contrario hacer la misma operación pero sólo para 'a'.*/
+    public static void exercise6() {
+        final int a = intInput("Ingrese un numero entero a");
+        final int b = intInput("Ingrese un numero entero b");
+
+        if (a > 0 && b > 0 && isEven(b + 1)) {
+            printTrue();
+        }
+
+        if (isEven(a + 1)) {
+            printTrue();
+        }
+    }
+
+/*   7. Ingresar tres números 'a', 'b' y 'c'. En caso de que 'a' sea positivo y la suma de 'b' y 'c' sea un número positivo,
+    mostrar 'true'. Si esto último no sucede pero la resta entre 'a' y 'b' es negativa, también mostrar 'true'.
+    En todos los demás casos indicar por pantalla si la resta de los tres números es igual a 0.*/
+    public static void exercise7() {
+        final int a = intInput("Ingrese un numero entero a");
+        final int b = intInput("Ingrese un numero entero b");
+        final int c = intInput("Ingrese un numero entero c");
+
+        if (isPositive(a)) {
+            if (isPositive(b + c) || isNegative(a - b)) {
+                printTrue();
+            }
+        }
+
+        if ((a - b - c) == 0) {
+            printTrue();
+        }
+    }
+
+    // ----------- Libraries --------------
+
+    private static boolean isPositive(int num) {
+        return num > 0;
+    }
+
+    private static boolean isNegative(int num) {
+        return num < 0;
+    }
+
+    private static boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    private static void printTrue() {
+        print(true);
     }
 }
