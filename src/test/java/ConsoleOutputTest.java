8bb4d7d213a450c5f71695d9842f963cbd2152b0
Ortola <nicolas.ortola@mercadolibre.com>
2021-09-19 22:10:14 -0300

    mejoro ConsoleOutputTest

--- src/test/java/ConsoleOutputTest.java
+++ src/test/java/ConsoleOutputTest.java
@@ -1,11 +1,9 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import util.IOUtil;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ConsoleOutputTest {
     private final PrintStream standardOut = System.out;
@@ -21,23 +19,50 @@ public class ConsoleOutputTest {
         System.setOut(standardOut);
     }
 
-    protected void assertConsoleOutput(String ... expectedOutputs) {
-        final String output = outputStreamCaptor.toString();
-
-        for (String expectedOutput : expectedOutputs) {
-            assertTrue(output.contains(expectedOutput));
+    protected void assertConsoleOutput(Object ... objects) {
+        for (Object object : objects) {
+            assertConsoleOutput(String.valueOf(object));
         }
     }
 
-    protected void assertConsoleDidNotOutput(String ... expectedOutputs) {
+    protected void assertConsoleOutput(String ... expectedOutputs) {
         final String output = outputStreamCaptor.toString();
 
+        IOUtil.print(output);
         for (String expectedOutput : expectedOutputs) {
-            assertFalse(output.contains(expectedOutput));
+            if (!output.contains(expectedOutput)) {
+                throw new ConsoleOutputException(
+                        String.format("Expected '%s' to be present in output but it was not! \nActual output was: \n%s", expectedOutput, output)
+                );
+            }
+        }
+    }
+
+    protected void assertConsoleDidNotOutput(Object ... objects) {
+        for (Object object : objects) {
+            assertConsoleDidNotOutput(String.valueOf(object));
+        }
+    }
+
+    protected void assertConsoleDidNotOutput(String ... unexpectedOutputs) {
+        final String output = outputStreamCaptor.toString();
+
+        for (String unexpectedOutput : unexpectedOutputs) {
+            if (output.contains(unexpectedOutput)) {
+                throw new ConsoleOutputException(
+                        String.format("Expected '%s' to NOT be present in output but it was! \nActual output was: \n%s", unexpectedOutput, output)
+                );
+            }
         }
     }
 
     protected void clearConsole() {
         outputStreamCaptor.reset();
+    }
+
+    private static final class ConsoleOutputException extends RuntimeException {
+        private ConsoleOutputException(String message) {
+            super(message);
+        }
     }
 }
